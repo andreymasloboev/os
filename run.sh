49da6b868c0fdcf46fc0d49e778a688fd334f790
Andrey Masloboev <amasloboev@topcon.com>
2025-01-12 17:15:48 +0300

    fixed start kernel

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
